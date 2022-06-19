d2486300f7730b1cff1078b7470aba0506f9636a
Jaedson Silva <contacanal798@gmail.com>
2022-06-19 16:30:52 -0300

    creating method _parser to get file config

--- pyconfparser/main.py
+++ pyconfparser/main.py
@@ -6,6 +6,33 @@ class PyConfigParser:
         self.file_content = None
         self._config = None
 
+    def _parser(self) -> None:
+        file_lines = self.file_content.split('\n')
+
+        current_section = None
+        file_config = {}
+
+        for line in file_lines:
+            line = line.strip()
+
+            if len(line) > 0:
+                # if is a section
+                if line.endswith(':'):
+                    section_name = line.replace(':', '')
+                    current_section = section_name
+                    file_config[section_name] = {}
+                elif '=' in line:  # if is a option                
+                    option, value = line.split('=')
+                    option = option.strip()
+                    value = value.strip()
+
+                    if not current_section:
+                        file_config[option] = value
+                    else:
+                        file_config[current_section][option] = value
+
+        self._config = file_config
+
     def read_file(self, filename: str) -> None:
         with open(filename, 'r') as file_r:
             self.file_content = file_r.read()
