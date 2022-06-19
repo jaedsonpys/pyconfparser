d5d06b6f5f71ff2140bf6fd52f257ea2f858b5ef
Jaedson Silva <contacanal798@gmail.com>
2022-06-19 16:13:43 -0300

    adding method to read a file using filename

--- pyconfparser/main.py
+++ pyconfparser/main.py
@@ -5,3 +5,7 @@ class PyConfigParser:
     def __init__(self):
         self.file_content = None
         self._config = None
+
+    def read_file(self, filename: str) -> None:
+        with open(filename, 'w') as file_w:
+            self.file_content = file_w.read()
